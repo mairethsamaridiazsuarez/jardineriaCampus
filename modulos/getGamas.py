586e47b9f1cf734b0fb8dcfcd83f1c8291f843c8
mairethsamaridiazsuarez <160806821+mairethsamaridiazsuarez@users.noreply.github.com>
2024-03-18 16:56:50 -0500

    json en gama

---
+++ modulos/getGamas.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+def getAllGama():
+    #json-server storage/producto.json -b 5002
+    peticion = requests.get("http://192.168.253.128:5002")
+    data = peticion.json()
+    return data
+    
